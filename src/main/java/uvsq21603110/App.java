01c2c0a92ea139b63485cc9d8a8bbd33be25aa7a
uvsq21603110 <alexandre.sarrazin@ens.uvsq.fr>
2020-03-02 13:04:14 +0100

    Premier commit

---
+++ src/main/java/uvsq21603110/App.java
@@ -0,0 +1,13 @@
+package uvsq21603110;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
